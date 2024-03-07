5b8890eee6089dd2b0f5987b6f0e5e7cefae5c8f
Zein Tonconi <zeintonconi@gmail.com>
2024-03-06 20:26:06 -0400

    Initial Configuration

---
+++ Backend/server.js
@@ -0,0 +1,62 @@
+// Variables de entorno
+// require('dotenv').config()
+
+const express = require('express')
+const cors = require('cors')
+// const sequelize = require('./database/config.js')
+
+
+// require('./models/index.models')
+
+
+
+class Server {
+
+  constructor () {
+    
+    this.port = 8080;
+    // this.port = process.env.APP_PORT
+    this.app = express()
+
+    // Path de mis servicios
+    // this.taskPath = '/api/tasks'
+    this.productsPath = '/api/products';
+
+    this.init()
+  }
+
+  async init () {
+    // await sequelize.sync({ force: false })
+    this.middlewares()
+    this.routes()
+  }
+
+
+  middlewares () {
+    this.app.use(cors())
+
+    this.app.use(express.json())
+
+    this.app.use(express.static('public'))
+  }
+
+//   getConnection = (req, res, next) => {
+//     const { pool, connection } = this.connection
+//     req.pool = pool
+//     req.connection = connection
+//     next()
+//   }
+
+  routes () {
+    
+    this.app.use(this.productsPath, require('./routes/products.routes'));
+  }
+
+  start () {
+    this.app.listen(this.port, () => {
+      console.log(`Server online on port ${this.port}`)
+    })
+  }
+}
+
+module.exports = Server
